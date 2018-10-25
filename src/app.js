f01f09cba91ca129ff81b61887d26454d1a8826b
Blake Watson <blake@kingdomrpg.com>
2018-10-25 15:21:17 -0700

    Remove dummy expense data.

--- src/app.js
+++ src/app.js
@@ -7,14 +7,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css' 
 
-
-import { addExpense } from './actions/expenses'
-
 const store = configureStore();
-
-store.dispatch(addExpense({description: 'Muffin tax', amount: 300, createdAt: 3500}));
-store.dispatch(addExpense({description: 'Water bill', amount: 800000, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 5500, createdAt: 5000}));
 
 const jsx = (
     <Provider store={store}>
